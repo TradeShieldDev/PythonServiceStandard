273d17fdfec2f7d58a19f3409682a23f7ec1f13f
Marius Vorster <marius.vrstr@gmail.com>
2025-03-23 08:54:54 +0200

    Update Scheduler

--- service_schedule_example/main.py
+++ service_schedule_example/main.py
@@ -1,23 +1,59 @@
 import asyncio
-from src.schedule import Scheduler
-from sdk.config.config_loader import ConfigType, ConfigManager
-from sdk.logging.logger import Logger
+import threading
 
-config = ConfigManager.get_instance()
+from src.schedule import Scheduler
+from sdk.config.config_loader import ConfigManager, DataType
+from sdk.logging.logger import Logger
+from src.config.sections import ConfigSections
+
+import select
+import sys
+import keyboard
+
+config:ConfigManager = ConfigManager.get_instance()
 logger = Logger.get_instance()
 
-def start():
-    logger.info('Scheduler service is starting up...')
+stop_event = threading.Event()
 
+def key_press_listener():
+    import keyboard
+    keyboard.wait('esc')
+    stop_event.set()
+
+
+async def start():
+
+    delay_in_seconds = config.get_ini_config_value(ConfigSections.ScheduleDetails, 'delay_in_seconds', 10, DataType.Integer)
+    
     ## Get delay from config
-    scheduler = Scheduler(10)
-    asyncio.run(scheduler.start())
+    scheduler = Scheduler(delay_in_seconds)
 
-    logger.info('Scheduler service has stopped.')
+    task = asyncio.create_task(scheduler.start())
+    print("Press 'ESC' key to stop...")
 
-start()
+    # Start the thread for listening to keyboard input
+    listener_thread = threading.Thread(target=key_press_listener)
+    listener_thread.start()
 
+    # Loop until the stop event is set
+    while not stop_event.is_set():
+        await asyncio.sleep(0.1)
 
+    task.cancel()
+    await task  # Wait for the task to be cancelled
+
+loop = asyncio.get_event_loop()
+
+try:
+    logger.info('Scheduler service is starting up...')
+    loop.run_until_complete(start())
+
+except Exception as ex:        
+    logger.error(f"Oops! {ex.__class__} occurred. Scheduler service has stopped. Details: {ex}") 
+
+finally:
+    loop.close()
+    logger.info('Scheduler service has stopped')
 
 
 
--- service_schedule_example/src/schedule.py
+++ service_schedule_example/src/schedule.py
@@ -2,19 +2,29 @@ import asyncio
 
 from typing import List
 from datetime import datetime
+from sdk.logging.logger import Logger
+
+logger = Logger.get_instance()
 
 class Scheduler:
 
     delay_in_seconds = None
+    counter = 0
 
     def __init__(self, delay_in_seconds:int):
         self.delay_in_seconds = delay_in_seconds
 
     async def exec(self):
-        print(f"Task is running at {datetime.utcnow()}")
+        self.counter += 1
+        logger.info(f"Task is running at {datetime.utcnow()}. Counter [{self.counter}] every [{self.delay_in_seconds}] seconds")
 
     async def start(self):
 
-        while True:
-            await self.exec()
-            await asyncio.sleep(self.delay_in_seconds)  # 5 minutes in seconds
+        try:
+
+            while True:
+                await self.exec()
+                await asyncio.sleep(self.delay_in_seconds)
+
+        except asyncio.CancelledError:
+            print("Task was cancelled")
